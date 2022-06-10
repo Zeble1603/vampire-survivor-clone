0ef9ed79ee3a45bae3889f08cbf02c219e7da2ad
Blaise <blaisedecarvalho@gmail.com>
2022-06-10 19:08:59 +0200

    moving bg & chara picture

--- src/index.js
+++ src/index.js
@@ -1,4 +1,83 @@
-const canvas = document.querySelector("#canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+canvas.width = 1024
+canvas.height = 576
+ctx.fillstyle = 'white'
 
-canvas.style.backgroundColor = "blue";
+
+const bgImage = new Image()
+bgImage.src = '/src/img/bg_forest.png'
+
+const charaImage = new Image()
+charaImage.src = '/src/img/chara/knight_f_idle_anim_f1.png'
+
+const background = new Sprite(
+    7000,6860,bgImage,{x:-2560,y:-2026}
+)
+
+const chara = new Sprite(
+    24,42,charaImage,{x:(canvas.width/2 - charaImage.width/2) ,y:(canvas.height/2-charaImage.height /2)}
+)
+
+const keys = {
+    up : false,
+    right : false,
+    down : false,
+    left : false, 
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    background.draw()
+    chara.draw()
+
+    if(keys.up){
+        background.position.y += 2
+    }
+    if(keys.right){
+        background.position.x -= 2
+    }
+    if(keys.down){
+        background.position.y -= 2
+    }
+    if(keys.left){
+        background.position.x += 2
+    }
+
+
+}
+animate()
+
+window.addEventListener('keydown', (e)=>{
+    switch (e.key) {
+        case "w":
+            keys.up = true
+            break;
+        case "a":
+            keys.left = true
+            break;   
+        case "d":
+            keys.right = true
+            break;   
+        case "s":
+            keys.down = true
+            break;     
+    }
+})
+
+window.addEventListener('keyup', (e)=>{
+    switch (e.key) {
+        case "w":
+            keys.up = false
+            break;
+        case "a":
+            keys.left = false
+            break;   
+        case "d":
+            keys.right = false
+            break;   
+        case "s":
+            keys.down = false
+            break;     
+    }
+})
--- src/models.js
+++ src/models.js
@@ -1,16 +1,26 @@
-class motherShape{
-    constructor(height,width,shape,x,y){
-        this.height = height
+class Sprite{
+    constructor(width,height,shape,position){
         this.width = width
+        this.height = height
         this.shape = shape
-        this.x = x
-        this.y = y
+        this.position = position
     }
+
+    draw(){
+        ctx.drawImage(this.shape,this.position.x,this.position.y,this.width,this.height)
+
+    }
+    
 }
 
-class player extends motherShape{
-    constructor(height,width,shape,x,y,stats,pv,level,xp,xpToNextLvl,attack,skills){
-        super(height,width,shape,x,y)
+class player extends Sprite{
+    constructor(height,width,shape,position,
+        stats,pv,level,xp,xpToNextLvl,attack,skills){
+        super(height,width,shape,position)
+        
+        this.shapeRight = shapeRight
+        this.shapeDown = shapeDown
+        this.shapeUp = shapeUp
         this.stats = stats
         this.pv = pv
         this.level = level
@@ -19,21 +29,62 @@ class player extends motherShape{
         this.attack = attack
         this.skills = skills
     }
+    /* METHODS
+    1/Move, different from mother
+    --> just flip the image from left to right
+    2/Erase
+    3/Draw
+    */
+    drawRight(){
+        ctx.drawImage(this.shapeRight,this.position.x,this.position.y,this.width,this.height)
+    }
+
+
+    move(key){
+        switch (key) {
+            case "ArrowLeft":
+                this.erase()
+                this.draw()
+                break;
+            case "ArrowRight":
+                this.erase()
+                this.drawRight()
+                break;
+            case "ArrowUp":
+                this.erase()
+                this.drawUp()
+                break;
+            case "ArrowDown":
+                this.erase()
+                this.drawDown()
+                break;
+        }
+    }
 }
 
-class enemy extends motherShape{
+class enemy extends Sprite{
     constructor(height,width,shape,x,y,pv,attack){
         super(height,width,shape,x,y)
         this.pv = pv
         this.attack = attack
     }
+    /* METHODS
+    1/Move
+    2/Erase 
+    3/Draw    
+    */
 }
 
-class object extends motherShape{
+class object extends Sprite{
     constructor(height,width,shape,x,y,effect){
         super(height,width,shape,x,y)
         this.effect = effect
     }
+    /* METHODS
+    1/Erase 
+    2/Draw
+    3/Apply effect (heal & XP)
+    */
 }
 
 class skills{
@@ -43,13 +94,29 @@ class skills{
         this.image = image
         this.lvl = lvl
         this.effect = effect
-    }
+    }  
+    /* METHODS
+    1/Apply effect (buff) 
+    2/Add to skills 
+    3/Lvl up 
+    */
+
 }
 
 class weapon extends skills{
-    constructor(name,description,image,lvl,effect,hitbox,speed){
+    constructor(height,width,shape,x,y,name,description,image,lvl,effect,speed){
     super(name,description,image,lvl,effect)
-    this.hitbox = hitbox
+    this.height = height
+    this.width = width
+    this.shape = shape
+    this.x = x
+    this.y = y
     this.speed = speed
     }
+    /* METHODS
+    1/Attack
+    --> draw / erase / move 
+    2/Add to skills 
+    3/Lvl up 
+    */
 }
